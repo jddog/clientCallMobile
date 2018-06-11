c9ea87856053c0352721a93eaa9389f588e0af0a
Jose Daniel Urueña Diaz <31295268+jddog@users.noreply.github.com>
2018-06-11 15:16:36 -0500

    fitst commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private qrScanner: QRScanner) {
+
+  }
+
+  abrirScanner(){
+    this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+
+       // start scanning
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+         this.qrScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+
+         //realizamos subscripcion con firebase para obseervar cambios en le estado del pedido
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
+  }
+
+}
